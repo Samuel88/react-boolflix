d0fc198e23226d8290971e2cb275e11cf83f17e2
Samuel Panicucci <samuel.panicucci@gmail.com>
2026-05-19 09:00:36 +0200

    Add TMDB utility function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
 import useDebounce from "../hooks/useDebounce";
+import { searchMovies } from "../utils/tmdb";
 
 function HomePage() {  
   const [searchTerm, setSearchTerm] = useDebounce("", 1000, (value) => {
-    console.log("Debounced value:", value);
+    searchMovies(value)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
   });
 
   return (
---
+++ src/utils/tmdb.js
@@ -0,0 +1,39 @@
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+const baseQueryParams = {
+    include_adult: false,
+    language: 'it-IT',
+};
+
+const getImageUrl = (path, size = 'original') => {
+    return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+
+const fetchFromTMDB = (endpoint) => {
+    const url = `${TMDB_BASE_URL}${endpoint}`;
+    return fetch(url, {
+        headers: {
+            Authorization: `Bearer ${TMDB_API_KEY}`,
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`TMDB API error: ${response.statusText}`);
+        }
+        return response.json();
+    });
+};
+
+const searchMovies = (query) => {
+    const params = new URLSearchParams({
+        ...baseQueryParams,
+        query,
+    });
+
+    return fetchFromTMDB(`/search/movie?${params}`);
+};
+
+export {
+    searchMovies
+};
